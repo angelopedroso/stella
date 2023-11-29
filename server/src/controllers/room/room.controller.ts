86035fa7205ca30fae3d5712a04563c45a5ba430
angelopedroso <solonasusonly0@gmail.com>
2023-11-29 15:24:41 -0300

    feat: created room controller

---
+++ server/src/controllers/room/room.controller.ts
@@ -0,0 +1,36 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common'
+import { Model } from 'mongoose'
+import { InjectModel } from '@nestjs/mongoose'
+import { Room, User } from 'src/models'
+
+@Controller('api/rooms')
+export class RoomsController {
+  constructor(@InjectModel(Room.name) private readonly model: Model<Room>) {}
+
+  @Get()
+  async findRandomly(@Body() language: User) {
+    return this.model.aggregate([
+      {
+        $match: {
+          status: 'waiting',
+          connectedUsers: 1,
+          native: language.learn,
+          learn: language.native,
+        },
+      },
+      { $sample: { size: 1 } },
+    ])
+  }
+
+  @Get('/:id')
+  findById(@Param('id') id: string) {
+    return this.model.findById(id)
+  }
+
+  @Post()
+  save(@Body() item: Room) {
+    return item._id
+      ? this.model.findByIdAndUpdate(item._id, item, { new: true })
+      : this.model.create(item)
+  }
+}

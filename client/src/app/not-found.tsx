2a4013e34a42ca69f55ba9e83305fdf4e50d1daa
angelopedroso <solonasusonly0@gmail.com>
2023-11-29 17:11:30 -0300

    feat: created not-found page

---
+++ client/src/app/not-found.tsx
@@ -0,0 +1,26 @@
+import { ErrorIcon } from '@/assets/error-icon'
+import Link from 'next/link'
+
+export default function NotFound() {
+  return (
+    <main className="flex h-screen items-center justify-center px-4 md:px-0">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <ErrorIcon />
+        <div className="-mt-10 flex flex-col items-center justify-center">
+          <h1 className="text-center text-lg font-bold text-slate-900 dark:text-slate-300 md:text-2xl">
+            Oops! Parece que você está num lugar desconhecido.
+          </h1>
+          <p className="text-sm font-semibold text-slate-800 dark:text-slate-200 md:text-lg">
+            Dê meia volta e volte para casa!
+          </p>
+        </div>
+        <Link
+          href="/"
+          className="w-fit rounded-lg bg-violet-500 px-5 py-3 text-slate-100 transition-colors hover:bg-violet-400"
+        >
+          Ir para casa
+        </Link>
+      </div>
+    </main>
+  )
+}

7c7c142ce4ab61b26bd1b7fe51dbd1e044b22359
angelopedroso <solonasusonly0@gmail.com>
2023-11-30 18:25:56 -0300

    feat: changed column connectedUsers type

--- server/src/models/room.model.ts
+++ server/src/models/room.model.ts
@@ -1,6 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
-import { ObjectId, Types } from 'mongoose'
-import { User } from './user.model'
+import { ObjectId } from 'mongoose'
 
 @Schema()
 export class Room {
@@ -15,8 +14,8 @@ export class Room {
   @Prop({ required: true })
   learn: string
 
-  @Prop({ type: [{ type: Types.ObjectId, ref: 'User' }], min: 0, max: 2 })
-  connectedUsers: User[]
+  @Prop({ min: 0, max: 2 })
+  connectedUsers: number
 }
 
 export const RoomSchema = SchemaFactory.createForClass(Room)

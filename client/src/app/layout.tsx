8740880c07965897a56f651eca014363e85c557f
angelopedroso <solonasusonly0@gmail.com>
2023-11-30 00:27:42 -0300

    feat: created theme provider

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { ThemeProvider } from '@/providers/theme.provider'
+import { Header } from '@/components/header/header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} flex h-screen flex-col px-20 py-8`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }

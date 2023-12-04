9db7a92d422c8e0edcbe1fac5ab7a6b6ed3eb959
angelopedroso <solonasusonly0@gmail.com>
2023-12-03 21:20:13 -0300

    feat: added link to redirect to home

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -1,14 +1,18 @@
 import Image from 'next/image'
 import { ToggleDarkMode } from '../toggleDarkMode/dark-button'
 import Logo from '@/assets/StellaLogo.png'
+import Link from 'next/link'
 
 export function Header() {
   return (
     <header className="flex items-center justify-between pb-8">
-      <div className="flex items-center justify-center">
+      <Link
+        href="/"
+        className="flex cursor-pointer items-center justify-center"
+      >
         <Image src={Logo} alt="logo" width={56} height={56} />
         <h1 className="-ml-3.5 text-xl font-bold tracking-normal">tella</h1>
-      </div>
+      </Link>
       <div className="flex gap-2">
         <ToggleDarkMode />
       </div>

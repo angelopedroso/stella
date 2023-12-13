b6ab2bd9287b8bdf191df4430e6035f600f4b587
angelopedroso <solonasusonly0@gmail.com>
2023-12-13 18:44:54 -0300

    feat: added socket context event

--- client/src/contexts/languageContext.tsx
+++ client/src/contexts/languageContext.tsx
@@ -2,6 +2,7 @@
 
 import { Language } from '@/components/forms/home/hooks/useHomeForm'
 import React, { createContext, useState } from 'react'
+import { Socket } from 'socket.io-client'
 
 type Provider = {
   children: React.ReactNode
@@ -10,15 +11,44 @@ type Provider = {
 type Context = {
   userConfig: Language
   setUserConfig: (data: Language) => void
+  setRoom: (room: string) => void
+  setSocket: (socket: Socket | undefined) => void
+  socket: Socket | undefined
+  room: string
+  handleSetNewMessage: (message: string) => void
+  skipped: boolean
+  setSkipped: (skipped: boolean) => void
 }
 
 export const LanguageContext = createContext<Context>({} as Context)
 
 export function LanguageProvider({ children }: Provider) {
   const [userConfig, setUserConfig] = useState<Language>({} as Language)
+  const [room, setRoom] = useState<string>('')
+  const [socket, setSocket] = useState<Socket | undefined>()
+  const [skipped, setSkipped] = useState<boolean>(false)
+
+  function handleSetNewMessage(message: string) {
+    socket?.emit('add-message', {
+      text: message,
+      roomId: room,
+    })
+  }
 
   return (
-    <LanguageContext.Provider value={{ userConfig, setUserConfig }}>
+    <LanguageContext.Provider
+      value={{
+        userConfig,
+        setUserConfig,
+        handleSetNewMessage,
+        setRoom,
+        room,
+        setSocket,
+        socket,
+        skipped,
+        setSkipped,
+      }}
+    >
       {children}
     </LanguageContext.Provider>
   )

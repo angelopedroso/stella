b93d1893fc9d03f044e26481402964b967ece684
angelopedroso <solonasusonly0@gmail.com>
2023-12-13 18:40:41 -0300

    feat/fix: created skip-chat-room & fix bugs

--- server/src/gateways/messages/messages.gateway.ts
+++ server/src/gateways/messages/messages.gateway.ts
@@ -6,24 +6,17 @@ import {
   WebSocketServer,
 } from '@nestjs/websockets'
 import { Server, Socket } from 'socket.io'
-import { InjectModel } from '@nestjs/mongoose'
-import { Model } from 'mongoose'
-import { Room, User } from '@/models'
+import { Room } from '@/models'
 import { Message } from '@/@types/message'
 import { RoomService } from '@/services/room/room.service'
-import { UserService } from '@/services/user/user.service'
 import { Logger } from '@nestjs/common'
+import { User } from '@/@types/user'
 
 @WebSocketGateway({ cors: '*' })
 export class MessagesGateway implements OnGatewayDisconnect, OnGatewayInit {
   private logger: Logger = new Logger('MessagesGateway')
 
-  constructor(
-    @InjectModel(Room.name) private readonly roomsModel: Model<Room>,
-    @InjectModel(User.name) private readonly usersModel: Model<User>,
-    private readonly roomService: RoomService,
-    private readonly userService: UserService,
-  ) {}
+  constructor(private readonly roomService: RoomService) {}
 
   @WebSocketServer()
   server: Server
@@ -39,79 +32,85 @@ export class MessagesGateway implements OnGatewayDisconnect, OnGatewayInit {
   async handleDisconnect(client: Socket) {
     this.logger.log(`Client disconnected: ${client.id}`)
 
-    const user = await this.usersModel.findOne({ clientId: client.id })
+    const room = await this.roomService.findUserByRoom(client.id)
 
-    if (user) {
-      const roomId = user.room._id
-
-      this.server.emit('users-changed', {
-        user,
+    if (room) {
+      client.broadcast.to(room._id.toString()).emit('users-changed', {
+        user: client.id,
         event: 'left',
       })
 
-      const room = await this.roomsModel.findById(roomId).exec()
-
       if (room.totalUsers < 2) {
-        await this.roomService.deleteRoom(roomId)
+        await this.roomService.deleteRoom(room._id)
       } else {
-        await this.roomService.removeUserFromRoom(room, user._id)
+        await this.roomService.removeUserFromRoom(room, client.id)
       }
 
-      await this.userService.deleteUser(user._id)
+      client.leave(room._id.toString())
     }
   }
 
   @SubscribeMessage('enter-chat-room')
-  async enterChatRoom(client: Socket, data: Room) {
-    let user = await this.userService.findByClientId(client.id)
+  async enterChatRoom(client: Socket, chatData: User) {
+    const room = await this.roomService.findUserByRoom(client.id)
 
-    if (!user) {
-      user = await this.userService.createUser({
-        learn: data.learn,
-        native: data.native,
-      })
+    if (room) return
+
+    const user = {
+      id: client.id,
+      native: chatData.native,
+      learn: chatData.learn,
     }
 
-    const dataRoom = await this.roomService.findRandomlyRoom(user)
+    let roomData: Partial<Room> = await this.roomService.findRandomlyRoom(user)
 
-    if (!dataRoom || dataRoom.totalUsers > 1) {
-      const data = await this.roomService.createRoom(user)
-
-      user.room = data
-
-      await this.userService.updateUser(user._id, user)
-
-      client.emit('chat-room-entered', data._id.toString())
-
-      client.join(data._id.toString())
-
-      client.broadcast
-        .to(data._id.toString())
-        .emit('users-changed', { event: 'joined' })
-
-      return
+    if (!roomData) {
+      roomData = await this.roomService.createRoom(user)
+    } else {
+      await this.roomService.addUserToRoom(roomData._id, user)
     }
 
-    user.room = dataRoom
+    client.emit('chat-room-entered', roomData._id.toString())
 
-    user = await this.userService.updateUser(user._id, user)
-
-    await this.roomService.addUserToRoom(dataRoom._id, user._id)
-
-    client.emit('chat-room-entered', dataRoom._id.toString())
-
-    client.join(dataRoom._id.toString())
+    client.join(roomData._id.toString())
 
     client.broadcast
-      .to(dataRoom._id.toString())
-      .emit('users-changed', { event: 'joined' })
+      .to(roomData._id.toString())
+      .emit('users-changed', { user: client.id, event: 'joined' })
   }
 
   @SubscribeMessage('add-message')
-  addMessage(message: Message) {
+  addMessage(client: Socket, message: Message) {
     this.server.in(message.roomId).emit('message', {
       text: message.text,
-      from: message.ownerId,
+      from: client.id,
     })
+  }
+
+  @SubscribeMessage('skip-chat-room')
+  async skipChatRoom(client: Socket, user: User) {
+    const formattedUser = {
+      id: client.id,
+      native: user.native,
+      learn: user.learn,
+    }
+
+    let newRoom = await this.roomService.findRandomlyRoom(formattedUser)
+
+    this.handleDisconnect(client)
+
+    if (newRoom) {
+      await this.roomService.addUserToRoom(newRoom._id, formattedUser)
+    } else {
+      newRoom = await this.roomService.createRoom(formattedUser)
+    }
+
+    client.join(newRoom._id.toString())
+
+    client.emit('chat-room-entered', newRoom._id.toString())
+
+    client.broadcast
+      .to(newRoom._id.toString())
+      .emit('users-changed', { user: client.id, event: 'joined' })
   }
 }

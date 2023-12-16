c5b20e0c94f38a285adeb3c2518510740d921416
angelopedroso <solonasusonly0@gmail.com>
2023-12-16 14:28:15 -0300

    fix: text chat page css style

--- client/src/app/chat/page.tsx
+++ client/src/app/chat/page.tsx
@@ -2,7 +2,7 @@ import { MessageBox } from '@/components/messageBox/messageBox'
 
 export default function TextChatRoom() {
   return (
-    <main className="-mx-8 flex flex-1 flex-col items-center justify-between overflow-hidden sm:-mx-14 md:justify-center lg:mx-auto lg:w-3/5">
+    <main className="-mx-8 flex flex-1 flex-col items-center justify-between overflow-hidden rounded-lg sm:-mx-14 md:justify-center md:border md:p-4 lg:mx-auto lg:w-3/5">
       <MessageBox />
     </main>
   )

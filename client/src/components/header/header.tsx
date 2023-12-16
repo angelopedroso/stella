db96d162b45396f1ed3284b98cc064a4c82d8b41
angelopedroso <solonasusonly0@gmail.com>
2023-12-16 14:27:24 -0300

    fix: replaced old logo

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -1,17 +1,13 @@
 import Image from 'next/image'
 import { ToggleDarkMode } from '../toggleDarkMode/dark-button'
-import Logo from '@/assets/StellaLogo.png'
+import Logo from '@/assets/logo.png'
 import Link from 'next/link'
 
 export function Header() {
   return (
     <header className="flex items-center justify-between pb-8">
-      <Link
-        href="/"
-        className="flex cursor-pointer items-center justify-center"
-      >
-        <Image src={Logo} alt="logo" width={56} height={56} />
-        <h1 className="-ml-3.5 text-xl font-bold tracking-normal">tella</h1>
+      <Link href="/" className="cursor-pointer">
+        <Image src={Logo} alt="logo" width={56} height={56} objectFit="cover" />
       </Link>
       <div className="flex gap-2">
         <ToggleDarkMode />

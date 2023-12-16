4d8e519ba6d74a10a229e6b9bf7ff18133ae58d3
angelopedroso <solonasusonly0@gmail.com>
2023-12-16 14:26:49 -0300

    feat: added room's slot visual

--- client/src/components/messageFlow/messageFlow.tsx
+++ client/src/components/messageFlow/messageFlow.tsx
@@ -3,30 +3,44 @@
 import { useMessage } from './hooks/useMessage'
 
 export function MessageFlow() {
-  const { messages, socket, messageRef } = useMessage()
+  const { messages, socket, messageRef, roomSlot } = useMessage()
+  const maxUsersInRoom = 2
 
   return (
-    <div className="flex w-full flex-1 flex-col gap-4 overflow-y-auto rounded-t-lg bg-background p-2 md:border md:border-b-0 md:p-4">
-      {messages.map((message, index) => {
-        return message?.user ? (
-          <span
-            className="mx-auto rounded-lg bg-secondary px-2 py-1 text-sm font-semibold text-gray-500"
-            key={index}
-            ref={messageRef}
-          >
-            Guest {message?.event === 'joined' ? 'entered' : 'left'}
-          </span>
-        ) : (
-          <div
-            data-from={message?.from === socket?.id}
-            className="w-fit max-w-[70%] self-start break-words rounded-xl bg-secondary px-5 py-4 data-[from=true]:self-end data-[from=true]:bg-primary data-[from=true]:text-white"
-            key={index}
-            ref={messageRef}
-          >
-            <p>{message?.text}</p>
-          </div>
-        )
-      })}
-    </div>
+    <>
+      {/* Users quantity logged into room */}
+      <div className="flex w-full items-center gap-2 border-b px-1 pb-2 md:border-none">
+        <div
+          data-slot={roomSlot !== maxUsersInRoom}
+          className="h-3 w-3 rounded-full bg-green-500 data-[slot=true]:bg-red-500"
+        />
+        <p className="text-slate-600">
+          {roomSlot}/{maxUsersInRoom}
+        </p>
+      </div>
+      {/* Messages chat */}
+      <div className="flex w-full flex-1 flex-col gap-4 overflow-y-auto bg-background p-1">
+        {messages.map((message, index) => {
+          return message?.user ? (
+            <span
+              className="mx-auto rounded-lg bg-secondary px-2 py-1 text-sm font-semibold text-gray-500"
+              key={index}
+              ref={messageRef}
+            >
+              Guest {message?.event}
+            </span>
+          ) : (
+            <div
+              data-from={message?.from === socket?.id}
+              className="w-fit max-w-[70%] self-start break-words rounded-xl bg-secondary px-5 py-4 data-[from=true]:self-end data-[from=true]:bg-primary data-[from=true]:text-white"
+              key={index}
+              ref={messageRef}
+            >
+              <p className="tracking-wide">{message?.text}</p>
+            </div>
+          )
+        })}
+      </div>
+    </>
   )
 }

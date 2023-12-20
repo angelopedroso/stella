c819dfc57d3e57d449c530b8ea1353e8ea2c2d4e
angelopedroso <solonasusonly0@gmail.com>
2023-12-20 19:15:56 -0300

    fix: changed chat page component

--- client/src/app/chat/page.tsx
+++ client/src/app/chat/page.tsx
@@ -1,9 +1,10 @@
-import { MessageBox } from '@/components/messageBox/messageBox'
+import { ChatPage } from '@/components/chat/box'
+import { MessageProvider } from '@/contexts/messageContext'
 
 export default function TextChatRoom() {
   return (
-    <main className="-mx-8 flex flex-1 flex-col items-center justify-between overflow-hidden rounded-lg sm:-mx-14 md:justify-center md:border md:p-4 lg:mx-auto lg:w-3/5">
-      <MessageBox />
-    </main>
+    <MessageProvider>
+      <ChatPage />
+    </MessageProvider>
   )
 }

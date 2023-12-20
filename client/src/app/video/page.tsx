8ef40c1418dd0814e66c899112baade1155f5242
angelopedroso <solonasusonly0@gmail.com>
2023-12-20 19:15:28 -0300

    feat: added components in VoiceChat page

--- client/src/app/video/page.tsx
+++ client/src/app/video/page.tsx
@@ -1,3 +1,17 @@
-export default function VoiceChatRoom({ params }: any) {
-  return <h1>{params.id}</h1>
+import { ChatVideo } from '@/components/video/chat'
+import { VideoScreen } from '@/components/video/screen'
+import { MediaQueryProvider } from '@/contexts/mediaQueryContext'
+import { MessageProvider } from '@/contexts/messageContext'
+
+export default function VoiceChatRoom() {
+  return (
+    <MessageProvider>
+      <MediaQueryProvider>
+        <main className="relative flex h-full flex-col justify-center gap-4 rounded-lg lg:flex-row lg:border lg:p-4">
+          <VideoScreen />
+          <ChatVideo />
+        </main>
+      </MediaQueryProvider>
+    </MessageProvider>
+  )
 }

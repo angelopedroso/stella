d78983a7d79a5b4141d63676d2b3ddd96ae16a13
angelopedroso <solonasusonly0@gmail.com>
2023-12-20 19:16:22 -0300

    feat: added suppressHydrationWarning & changed import path

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.scss'
 import { ThemeProvider } from '@/providers/theme.provider'
-import { Header } from '@/components/header/header'
+import { Header } from '@/components/header'
 import { LanguageProvider } from '@/contexts/languageContext'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className} flex h-screen flex-col px-8 py-4 sm:px-14 sm:py-5 md:px-20 md:py-8`}
       >

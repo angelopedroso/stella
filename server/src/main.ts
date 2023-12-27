c8e4f6fd0674fee6d1e578ec0411654367f3a6d4
angelopedroso <solonasusonly0@gmail.com>
2023-12-27 18:16:16 -0300

    feat: added peer server

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { PeerServer } from 'peer'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  PeerServer({ port: 9001, path: '/peer' })
   app.enableCors({
     origin: '*',
   })

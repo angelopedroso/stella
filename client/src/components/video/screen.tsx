3a3bfcba028f1d9561c92bf45a16b474d5a63bc3
angelopedroso <solonasusonly0@gmail.com>
2023-12-27 18:32:46 -0300

    ♻️ refactor: added useVideo hook to VideoScreen component

--- client/src/components/video/screen.tsx
+++ client/src/components/video/screen.tsx
@@ -1,31 +1,33 @@
 'use client'
 
 import { useMediaQueryContext } from '@/hooks/useMediaQueryContext'
-import { useRef } from 'react'
 import Draggable from 'react-draggable'
+import { useVideo } from './hooks/useVideo'
 
 export function VideoScreen() {
   const { isDesktop, position } = useMediaQueryContext()
-  const myVideo = useRef<HTMLVideoElement>(null)
+  const { myVideoRef, guestVideoRef } = useVideo()
 
   return (
-    <div className="relative flex h-full flex-col gap-4 md:h-72 md:flex-row md:justify-center lg:h-full lg:flex-col">
+    <div className="relative flex h-full flex-col gap-4 md:h-72 md:flex-row-reverse md:justify-center lg:h-full lg:flex-col">
       <video
         autoPlay
         playsInline
-        className="w-full grow rounded-lg border bg-blue-500"
+        className="w-full grow rounded-lg border object-cover"
+        ref={guestVideoRef}
       />
       <Draggable
         bounds="parent"
         disabled={isDesktop}
         position={position}
-        nodeRef={myVideo}
+        nodeRef={myVideoRef}
       >
         <video
           autoPlay
           playsInline
-          className="absolute right-2 top-2 h-32 w-24 grow rounded-lg border bg-red-500 md:static md:h-auto md:w-full"
-          ref={myVideo}
+          className="absolute right-2 top-2 h-32 w-24 grow rounded-lg border object-cover md:static md:w-full"
+          ref={myVideoRef}
+          muted
         />
       </Draggable>
     </div>

690904e93c9c88982baeeed6b5bed32bffdf1c3d
angelopedroso <solonasusonly0@gmail.com>
2023-12-28 18:43:58 -0300

    💄feat: added menu toggle component to disable mic and cam

--- client/src/components/video/screen.tsx
+++ client/src/components/video/screen.tsx
@@ -1,34 +1,72 @@
 'use client'
 
-import { useMediaQueryContext } from '@/hooks/useMediaQueryContext'
 import Draggable from 'react-draggable'
+
 import { useVideo } from './hooks/useVideo'
+import { useMediaQueryContext } from '@/hooks/useMediaQueryContext'
+
+import { MenuUtils } from '@/components/ui/menu-util'
+
+import { Loader2, Mic, MicOff, Video, VideoOff } from 'lucide-react'
+import { Toggle } from '../ui/toggle'
+import { useVideoMenu } from './hooks/useVideoMenu'
 
 export function VideoScreen() {
   const { isDesktop, position } = useMediaQueryContext()
-  const { myVideoRef, guestVideoRef } = useVideo()
+  const { myVideoRef, guestVideoRef, isSearching } = useVideo()
+  const { checkedMic, handleToggleMic, checkedVideo, handleToggleVideo } =
+    useVideoMenu()
 
   return (
     <div className="relative flex h-full flex-col gap-4 md:h-72 md:flex-row-reverse md:justify-center lg:h-full lg:flex-col">
-      <video
-        autoPlay
-        playsInline
-        className="w-full grow rounded-lg border object-cover"
-        ref={guestVideoRef}
-      />
+      {!isSearching ? (
+        <video
+          autoPlay
+          playsInline
+          className="w-full grow rounded-lg border object-cover"
+          ref={guestVideoRef}
+          aria-label="Guest video cam"
+        >
+          Your browser does not support the video tag.
+        </video>
+      ) : (
+        <div className="flex w-full grow flex-col items-center justify-center rounded-lg border text-secondary">
+          <Loader2 className="h-1/2 w-1/2 animate-spin" />
+        </div>
+      )}
+
       <Draggable
         bounds="parent"
         disabled={isDesktop}
         position={position}
         nodeRef={myVideoRef}
       >
-        <video
-          autoPlay
-          playsInline
-          className="absolute right-2 top-2 h-32 w-24 grow rounded-lg border object-cover md:static md:w-full"
-          ref={myVideoRef}
-          muted
-        />
+        <div className="absolute right-2 top-2 h-32 w-24 grow overflow-hidden rounded-lg border md:static md:h-auto md:w-full">
+          <video
+            autoPlay
+            playsInline
+            className="h-full bg-red-500 object-cover"
+            ref={myVideoRef}
+            aria-label="Your video cam"
+            muted
+          />
+          <MenuUtils>
+            <Toggle
+              className="rounded-none hover:bg-primary/40 hover:text-foreground data-[state=on]:bg-primary/60"
+              aria-label="Mic toggle button"
+              onPressedChange={handleToggleMic}
+            >
+              {checkedMic ? <MicOff /> : <Mic />}
+            </Toggle>
+            <Toggle
+              className="rounded-none hover:bg-primary/40 hover:text-foreground data-[state=on]:bg-primary/60"
+              aria-label="Video toggle button"
+              onPressedChange={handleToggleVideo}
+            >
+              {checkedVideo ? <VideoOff /> : <Video />}
+            </Toggle>
+          </MenuUtils>
+        </div>
       </Draggable>
     </div>
   )

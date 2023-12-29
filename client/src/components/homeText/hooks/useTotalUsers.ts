b704314be0dd90854674696648e9ed6be5f5fea1
angelopedroso <solonasusonly0@gmail.com>
2023-12-28 22:15:39 -0300

    💄feat: created total users hook

---
+++ client/src/components/homeText/hooks/useTotalUsers.ts
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+import { useSocket, useLanguageContext } from '@/hooks'
+
+export function useTotalUsers() {
+  const socketURL = `${process.env.NEXT_PUBLIC_DATABASE_URL_PORT}`
+  const socket = useSocket(socketURL)
+
+  const { setSocket } = useLanguageContext()
+
+  const [totalUsers, setTotalUsers] = useState(0)
+
+  useEffect(() => {
+    socket?.on('total-users', setTotalUsers)
+    setSocket(socket)
+  }, [socket, setSocket])
+
+  return { totalUsers }
+}

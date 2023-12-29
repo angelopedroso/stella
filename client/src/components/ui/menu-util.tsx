9833dfee449d8c236574fd21e545b51abf8fd14b
angelopedroso <solonasusonly0@gmail.com>
2023-12-29 14:10:30 -0300

    💄feat: created MenuUtilsToggle component

--- client/src/components/ui/menu-util.tsx
+++ client/src/components/ui/menu-util.tsx
@@ -1,5 +1,9 @@
-import React, { ButtonHTMLAttributes, ReactNode } from 'react'
+import React, { ButtonHTMLAttributes, ComponentProps, ReactNode } from 'react'
 import { Button } from './button'
+
+import * as TogglePrimitive from '@radix-ui/react-toggle'
+import { Toggle } from './toggle'
+import { twMerge } from 'tailwind-merge'
 
 export type MenuUtilsProps = {
   variant?: 'mobile' | 'desktop'
@@ -9,6 +13,10 @@ export type MenuUtilsProps = {
 type MenuUtilsButtonProps = {
   children: ReactNode
 } & ButtonHTMLAttributes<HTMLButtonElement>
+
+type MenuUtilsToggleProps = {
+  children: ReactNode
+} & ComponentProps<typeof TogglePrimitive.Root>
 
 export function MenuUtils({ children, variant = 'desktop' }: MenuUtilsProps) {
   const isMobile = variant === 'mobile'
@@ -24,13 +32,27 @@ export function MenuUtils({ children, variant = 'desktop' }: MenuUtilsProps) {
 }
 
 export function MenuUtilsButton({ children, ...props }: MenuUtilsButtonProps) {
+  const buttonStyle = twMerge(
+    'h-12 w-12 rounded-none hover:bg-primary/60',
+    props.className,
+  )
+
   return (
-    <Button
-      variant="ghost"
-      className="h-12 w-12 rounded-none hover:bg-primary/60"
-      {...props}
-    >
+    <Button variant="ghost" className={buttonStyle} {...props}>
       {children}
     </Button>
+  )
+}
+
+export function MenuUtilsToggle({ children, ...props }: MenuUtilsToggleProps) {
+  const toggleStyle = twMerge(
+    'h-12 w-12 rounded-none px-4 py-2 hover:bg-primary/40 hover:text-foreground data-[state=on]:bg-primary/60',
+    props.className,
+  )
+
+  return (
+    <Toggle className={toggleStyle} {...props}>
+      {children}
+    </Toggle>
   )
 }

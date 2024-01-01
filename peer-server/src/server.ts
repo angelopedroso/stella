97d2336a5f9c6441d7b85a8f9e6b0337f1d5c3c8
angelopedroso <solonasusonly0@gmail.com>
2024-01-01 17:03:38 -0300

    test commit

--- peer-server/src/server.ts
+++ peer-server/src/server.ts
@@ -4,12 +4,4 @@ import { peerConfig } from "./config"
 
 PeerServer(peerConfig, (server) => {
  console.log("Peer Server Started")
-
- server.on("connection", (client) => {
-  console.log(`Client connected`)
- })
-
- server.on("disconnect", (client) => {
-  console.log(`Client disconnected`)
- })
 })
